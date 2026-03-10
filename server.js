c8cc3b7b6b9a2e01ea1c9b2ba243c06cfdb4e84b
m0544195828 <M0544195828@GMAIL.COM>
2026-03-11 00:45:26 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
+});
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
